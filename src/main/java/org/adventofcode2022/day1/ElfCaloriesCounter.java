a975df4468ed014bd632932da2847643e988224b
Mani Challa <mani.challa1990@gmail.com>
2023-11-29 11:28:45 +0000

    fixing threads excercise4

--- src/main/java/org/adventofcode2022/day1/ElfCaloriesCounter.java
+++ src/main/java/org/adventofcode2022/day1/ElfCaloriesCounter.java
@@ -2,6 +2,7 @@ package org.adventofcode2022.day1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,17 +10,28 @@ public class ElfCaloriesCounter {
 
     private List<Elf> elfList = new ArrayList<>();
 
-    public ElfCaloriesCounter(BufferedReader bufferedReader) throws IOException {
+    public ElfCaloriesCounter(BufferedReader bufferedReader) {
         String line;
         Elf elf = new Elf();
         elfList.add(elf);
-        while ( (line = bufferedReader.readLine()) !=null){
-            if(line.isBlank()){
-                elf = new Elf();
-                elfList.add(elf);
-            }else{
-                elf.addCalories(Integer.parseInt(line));
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.isBlank()) {
+                    elf = new Elf();
+                    elfList.add(elf);
+                } else {
+                    elf.addCalories(Integer.parseInt(line));
+                }
             }
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
         }
+    }
+
+    public int maxCalories(){
+        return elfList.stream()
+                .max(Elf::compareTo)
+                .get()
+                .getTotalCalories();
     }
 }
