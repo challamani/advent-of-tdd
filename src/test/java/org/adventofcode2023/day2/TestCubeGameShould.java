192d5c6763ad6aed18dfda2f9b4d78464f644c0c
Mani Challa <mani.challa1990@gmail.com>
2023-12-03 10:57:38 +0000

    AOC 2023 Day2 part1 tests

---
+++ src/test/java/org/adventofcode2023/day2/TestCubeGameShould.java
@@ -0,0 +1,44 @@
+package org.adventofcode2023.day2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestCubeGameShould {
+
+    @Test
+    void have_valid_game_id_on_cube_game_instance_created(){
+        fail("Not yet implemented!");
+    }
+
+    @Test
+    void return_zero_green_cubes_on_cube_game_instance_created(){
+        fail("Not yet implemented!");
+    }
+
+    @Test
+    void return_zero_red_cubes_on_cube_game_instance_created(){
+        fail("Not yet implemented!");
+    }
+
+    @Test
+    void return_zero_blue_cubes_on_cube_game_instance_created(){
+        fail("Not yet implemented!");
+    }
+
+    @Test
+    void return_10_blue_cubes_when_blue_cubes_are_added_to_game_instance(){
+        fail("Not yet implemented!");
+    }
+
+    @Test
+    void return_20_red_cubes_when_red_cubes_are_added_to_game_instance(){
+        fail("Not yet implemented!");
+    }
+
+    @Test
+    void return_30_green_cubes_when_green_cubes_are_added_to_game_instance(){
+        fail("Not yet implemented!");
+    }
+
+}
